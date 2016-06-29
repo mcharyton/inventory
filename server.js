7ac0257d4c2729924c2b32e424c41cf7e9df186a
mcharyton <charytom@ee.pw.edu.pl>
2016-06-29 15:20:55 +0200

    Added angular

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.sendfile('/src/index.html');
+    res.sendFile(__dirname + '/src/index.html');
 });
-
+app.use('/css', express.static(__dirname + '/src/css'));
+app.use('/images', express.static(__dirname + '/src/images'));
+app.use('/js', express.static(__dirname + '/src/js'));
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
