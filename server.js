18ee6e52a7470045442ffee7aaa1cfaa56cc3e75
mcharyton <charytom@ee.pw.edu.pl>
2016-08-30 21:23:39 +0200

    Structure modified

--- server.js
+++ server.js
@@ -3,13 +3,13 @@
  */
 var express = require('express');
 var app = express();
+// var mysql = require('src/msyql');
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/src/index.html');
-});
-app.use('/css', express.static(__dirname + '/src/css'));
-app.use('/images', express.static(__dirname + '/src/images'));
-app.use('/js', express.static(__dirname + '/src/js'));
+app.use(express.static(__dirname + '/public'));
+
+// app.get('/', function (req, res) {
+//     res.sendFile(__dirname + '/public/index.html');
+// });
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
