2045744b2be6bd8bca5ad10dcc147f9d5fa51ad7
fevrite <d.jakubiak94@gmail.com>
2016-09-29 11:16:41 +0200

    change name in status.html, change form's in orderForm

--- public/js/directives/productOrderForm.js
+++ public/js/directives/productOrderForm.js
@@ -3,39 +3,42 @@
  */
 app.directive('productOrderForm', function () {
     return {
+        // scope: {
+        //     data: '='
+        // },
+        //
         template: '<div> \
         <fieldset class="form-group">\
-        <label for="exampleSelect1">Kategoria</label>\
-        <select class="form-control" id="exampleSelect1">\
-        <option>1</option>\
-        <option>2</option>\
-        <option>3</option>\
-        <option>4</option>\
-        <option>5</option>\
+        <label for="categorySelect">Kategoria</label>\
+        <select name="category" class="form-control" id="categorySelect" ng-model="selectedCategory"">\
+            <option ng-repeat="category in categories" value="{{category.Category_Id}}">{{ category.Category_Name }}</option>\
+        </select>\
+        </fieldset>\
+        <fieldset class="form-group" ng-show="selectedCategory">\
+        <label for="typeSelect">Typ</label>\
+        <select name="type" class="form-control" id="typeSelect" ng-model="selectedType">\
+        <option ng-repeat="type in types | filter : { Category_Id: selectedCategory }" value="{{ type.Type_Id }}">{{ type.Type_Name }}</option>\
         </select>\
         </fieldset>\
         <fieldset class="form-group">\
-        <label for="exampleSelect1">Typ</label>\
-        <select class="form-control" id="exampleSelect1">\
-        <option>1</option>\
-        <option>2</option>\
-        <option>3</option>\
-        <option>4</option>\
-        <option>5</option>\
-        </select>\
+        <label for="number">Ilość</label>\
+        <input type="number" min="0" max="1000" class="form-control" id="quantity" ng-model="quantity">\
         </fieldset>\
         <fieldset class="form-group">\
-        <label for="number">Ilosc</label>\
-        <input type="number" ng-model="Quantity" class="form-control" id="ilosc" placeholder="">\
+        <label for="fromDate">Data zamówienia </label>\
+        {{today}}\
+    <input type="date" min="{{ today |date: \'yyyy-MM-dd\' }}" class="form-control" id="fromDate" ng-model="fromDate" name="fromName">\
         </fieldset>\
         <fieldset class="form-group">\
-        <label for="date">Data </label>\
-        <input type="date" ng-model="Order_Date" class="form-control" id="date1" placeholder="">\
+        <label for="toDate">Data żądanej realizacji </label>\
+        {{maxDate}}\
+    <input type="date" class="form-control" id="toDate" ng-model="toDate" name="toDate" min="{{ today |date: \'yyyy-MM-dd\' }}" >\
         </fieldset>\
         <fieldset class="form-group">\
         <label for="exampleTextarea">Uzasadnienie</label>\
         <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>\
         </fieldset>\
-            </div>'
+        </div>'
     };
 });
+
